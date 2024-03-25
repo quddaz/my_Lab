14757302e8db036728d22af64b78a01c459908e5
김병웅 <quddnddl35@naver.com>
2024-03-25 17:37:25 +0900

    JWT 시작 0325

---
+++ Spring Security + JWT/src/test/java/quddaz/jwtTest/JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package quddaz.jwtTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
