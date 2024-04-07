ca8d7a99cc52977237f74295be04b1935593ae1e
김병웅 <quddnddl35@naver.com>
2024-04-07 18:00:07 +0900

    JWT 완료 0407

--- Spring Security + JWT/src/main/java/quddaz/jwtTest/Entity/DTO/CustomUserDetails.java
+++ Spring Security + JWT/src/main/java/quddaz/jwtTest/Entity/DTO/CustomUserDetails.java
@@ -2,11 +2,13 @@ package quddaz.jwtTest.Entity.DTO;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import quddaz.jwtTest.Entity.UserEntity;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * 스프링 시큐리티에서 사용자의 정보를 표현하는데 사용
@@ -19,19 +21,14 @@ public class CustomUserDetails implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
+    // 사용자가 가진 권한을 담을 리스트
+    List<GrantedAuthority> authorities = new ArrayList<>();
 
-    Collection<GrantedAuthority> collection = new ArrayList<>();
+    // 사용자 엔티티로부터 권한 정보를 가져와서 권한 객체로 변환하여 리스트에 추가
+    String role = userEntity.getRole();
+    authorities.add(new SimpleGrantedAuthority(role));
 
-    collection.add(new GrantedAuthority() {
-
-      @Override
-      public String getAuthority() {
-
-        return userEntity.getRole();
-      }
-    });
-
-    return collection;
+    return authorities;
   }
 
   @Override
