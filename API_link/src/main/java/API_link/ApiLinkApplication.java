25ebd54a7966acff8eb39046c2d0e72833d13995
김병웅 <quddnddl35@naver.com>
2024-04-29 20:27:26 +0900

    0429

---
+++ API_link/src/main/java/API_link/ApiLinkApplication.java
@@ -0,0 +1,13 @@
+package API_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLinkApplication.class, args);
+	}
+
+}
