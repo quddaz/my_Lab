0e08eabbb8b1303bc38daf45cce079eba216a67e
김병웅 <quddnddl35@naver.com>
2024-05-05 15:48:48 +0900

    0505 API 링크 테스트

--- API_link/src/main/java/API_link/Api_LinkTest.java
+++ API_link/src/main/java/API_link/Api_LinkTest.java
@@ -1,85 +1,80 @@
 package API_link;
 
-import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.net.URLEncoder;
+
 @RestController
 public class Api_LinkTest {
-  @Value("${openApi.serviceKey}")
-  private String serviceKey;
-
-  @Value("${openApi.callBackUrl}")
-  private String callBackUrl;
-
-  @Value("${openApi.dataType}")
-  private String dataType;
 
   @GetMapping("/forecast")
   public ResponseEntity<String> callForecastApi(
-      @RequestParam(value="base_time") String baseTime,
-      @RequestParam(value="base_date") String baseDate,
-      @RequestParam(value="beach_num") String beachNum
-  ){
-    HttpURLConnection urlConnection = null;
-    InputStream stream = null;
-    String result = null;
-
-    String urlStr = callBackUrl +
-        "serviceKey=" + serviceKey +
-        "&dataType=" + dataType +
-        "&base_date=" + baseDate +
-        "&base_time=" + baseTime +
-        "&beach_num=" + beachNum;
-
-    try {
-      URL url = new URL(urlStr);
-
-      urlConnection = (HttpURLConnection) url.openConnection();
-      stream = getNetworkConnection(urlConnection);
-      result = readStreamToString(stream);
-
-      if (stream != null) stream.close();
-    } catch(IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (urlConnection != null) {
-        urlConnection.disconnect();
-      }
-    }
-
-    return new ResponseEntity<>(result, HttpStatus.OK);
-  }
-
-  /* URLConnection 을 전달받아 연결정보 설정 후 연결, 연결 후 수신한 InputStream 반환 */
-  private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
-    urlConnection.setConnectTimeout(3000);
-    urlConnection.setReadTimeout(3000);
-    urlConnection.setRequestMethod("GET");
-    urlConnection.setDoInput(true);
-
-    if(urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-      throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
-    }
-
-    return urlConnection.getInputStream();
-  }
-
-  /* InputStream을 전달받아 문자열로 변환 후 반환 */
-  private String readStreamToString(InputStream stream) throws IOException{
+      @RequestParam(value = "rsdAreaCd") String rsdArea,
+      @RequestParam(value = "ageCd") String ageCd,
+      @RequestParam(value = "closStdrYm") String closStdrYm
+  ) {
     StringBuilder result = new StringBuilder();
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+    try {
+      String apiUrl = "https://eis.work.go.kr/opi/joApi.do";
+      String apiSecd = "OPIA";
+      String sxdsCd = "M";
+      int bgnPage = 1;
+      int display = 10;
 
-    String readLine;
-    while((readLine = br.readLine()) != null) {
-      result.append(readLine + "\n\r");
+      // URL 구성
+      String urlStr = apiUrl + "?apiSecd=" + URLEncoder.encode(apiSecd, "UTF-8")
+          + "&rsdAreaCd=" + URLEncoder.encode(rsdArea, "UTF-8")
+          + "&sxdsCd=" + URLEncoder.encode(sxdsCd, "UTF-8")
+          + "&ageCd=" + URLEncoder.encode(ageCd, "UTF-8")
+          + "&rernSecd=XML"
+          + "&closStdrYm=" + URLEncoder.encode(closStdrYm, "UTF-8")
+          + "&bgnPage=" + bgnPage
+          + "&display=" + display;
+
+      URL url = new URL(urlStr);
+
+      HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+      urlConnection.setConnectTimeout(3000);
+      urlConnection.setReadTimeout(3000);
+      urlConnection.setRequestMethod("GET");
+      urlConnection.setDoInput(true);
+
+      if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+        InputStream inputStream = urlConnection.getInputStream();
+        result.append(readStreamToString(inputStream, "EUC-KR"));
+      } else {
+        throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
+      }
+
+      urlConnection.disconnect();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    br.close();
+    return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+  }
+
+  // InputStream을 문자열로 변환하는 메서드
+  private String readStreamToString(InputStream stream, String charset) throws IOException {
+    StringBuilder result = new StringBuilder();
+
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(stream, charset))) {
+      String readLine;
+      while ((readLine = br.readLine()) != null) {
+        result.append(readLine).append("\n");
+      }
+    }
 
     return result.toString();
   }
